e15642b378d940ac9d14cdaa3b1ebe2e7f9c8a8e
Dayer <93074807+dayercher005@users.noreply.github.com>
2026-02-14 15:47:36 +0800

    feat: consolidated main pages of portfolio

---
+++ src/pages/Portfolio.tsx
@@ -0,0 +1,14 @@
+import { Background } from "@/components/background.tsx";
+import { AnimatedBackground } from "@/components/animated-background.tsx";
+
+function Portfolio() {
+
+  return (
+    <>
+      <Background />
+      <AnimatedBackground />
+    </>
+  )
+}
+
+export default Portfolio
