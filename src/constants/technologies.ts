c2146fd8a42bfd9e94ebfb606906e4ed50f96c9c
Dayer <93074807+dayercher005@users.noreply.github.com>
2026-02-15 01:33:26 +0800

    feat: separated constant technologies

---
+++ src/constants/technologies.ts
@@ -0,0 +1,12 @@
+export const Technology = {
+    EXPRESS: "Express.js",
+    NODEJS: "Node.js",
+    POSTGRESQL: "PostgreSQL",
+    PRISMA: "Prisma",
+    PYTHON: "Python",
+    REACT: "React",
+    TAILWIND: "Tailwind",
+    TYPESCRIPT: "Typescript",
+} as const
+
+export type TechnologyTypes = typeof Technology[keyof typeof Technology]
