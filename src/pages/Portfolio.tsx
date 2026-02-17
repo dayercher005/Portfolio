37db5db757825539800e1da7b7a95855a3bd72da
Dayer <93074807+dayercher005@users.noreply.github.com>
2026-02-18 00:18:09 +0800

    chore: minor ui changes to pages

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -1,13 +1,15 @@
 import { Background } from "@/components/background.tsx";
+import { Navbar } from "@/components/navbar/navbar.tsx";
 import { Hero } from "@/components/hero/hero.tsx";
 import { Skills } from '@/components/skills/skills.tsx';
-import { Education } from '@/components/education/education.tsx'
+import { Education } from '@/components/education/education.tsx';
 
 function Portfolio() {
 
   return (
     <>
       <Background />
+      <Navbar />
       <Hero />
       <Education />
       <Skills />
