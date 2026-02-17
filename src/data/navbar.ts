79776d96617772b5933cc2e0ee7be72ed37eea47
Dayer <93074807+dayercher005@users.noreply.github.com>
2026-02-18 00:17:44 +0800

    feat: shifted github and linkedin data to navbar constant file

---
+++ src/data/navbar.ts
@@ -0,0 +1,13 @@
+interface NavbarData {
+    title: string
+    github: string
+    linkedin: string
+    resume: string
+}
+
+export const NavbarData = {
+    title: "Dayer Cher",
+    github: "https://github.com/dayercher005",
+    linkedin: "https://www.linkedin.com/in/dayer-cher-9a9a14231/",
+    resume: "",
+}
