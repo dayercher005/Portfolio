359529991178e2259b11f8ab798a191f493ac50e
Dayer <dayerxr.cher@gmail.com>
2026-03-23 22:02:20 +0800

    chore: minor edits to styling

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -3,6 +3,7 @@ import { Navbar } from "@/components/navbar/navbar.tsx";
 import { Hero } from "@/components/hero/hero.tsx";
 import { Skills } from '@/components/skills/skills.tsx';
 import { Education } from '@/components/education/education.tsx';
+import { Experience } from '@/components/experience/experience.tsx'
 
 function Portfolio() {
 
@@ -11,6 +12,7 @@ function Portfolio() {
       <Background />
       <Navbar />
       <Hero />
+      <Experience />
       <Education />
       <Skills />
     </>
