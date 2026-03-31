21fc04ca7fb3a90c539a9b6a64fbaf03ba2d0adf
Dayer <dayerxr.cher@gmail.com>
2026-03-31 22:31:27 +0800

    chore: updated types and props

--- src/data/navbar.ts
+++ src/data/navbar.ts
@@ -2,12 +2,14 @@ interface NavbarData {
     title: string
     github: string
     linkedin: string
-    resume: string
+    resumePath: string
+    resumeName: string
 }
 
 export const NavbarData = {
     title: "Dayer Cher",
     github: "https://github.com/dayercher005",
     linkedin: "https://www.linkedin.com/in/dayer-cher-9a9a14231/",
-    resume: "",
+    resumePath: "./src/assets/resume/resume.pdf",
+    resumeName: "resume.pdf"
 }
