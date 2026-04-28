939202fa6d7add83f3a236e6ea0d1d7a49cd9ec7
Dayer <dayerxr.cher@gmail.com>
2026-04-28 16:09:30 +0800

    chore: updated resume link

--- src/data/navbar.ts
+++ src/data/navbar.ts
@@ -10,6 +10,6 @@ export const NavbarData = {
     title: "Dayer Cher",
     github: "https://github.com/dayercher005",
     linkedin: "https://www.linkedin.com/in/dayer-cher-9a9a14231/",
-    resumePath: "https://drive.google.com/file/d/1ukuVCjl65Bj0vtt0ez7fhNKwYSCLsxvK/view?usp=drive_link",
+    resumePath: "https://drive.google.com/file/d/1xtFQjtTxfU3H4LBqNP-D6kJB0HILWZ8x/view?usp=drive_link",
     resumeName: "resume.pdf"
 }
