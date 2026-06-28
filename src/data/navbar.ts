c6d2f2f567e4703655917059efe214ab25b1fb25
Dayer <dayerxr.cher@gmail.com>
2026-06-28 23:07:42 +0800

    feat: updated linkedin url

--- src/data/navbar.ts
+++ src/data/navbar.ts
@@ -9,7 +9,7 @@ interface NavbarData {
 export const NavbarData = {
     title: "Dayer Cher",
     github: "https://github.com/dayercher005",
-    linkedin: "https://www.linkedin.com/in/dayer-cher-9a9a14231/",
+    linkedin: "https://www.linkedin.com/in/dayer-c-9a9a14231/",
     resumePath: "https://drive.google.com/file/d/1xtFQjtTxfU3H4LBqNP-D6kJB0HILWZ8x/view?usp=drive_link",
     resumeName: "resume.pdf"
 }
